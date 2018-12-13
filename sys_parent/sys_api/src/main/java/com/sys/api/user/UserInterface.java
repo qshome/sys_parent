9755b8bcbbb77208b86358afd8b500af6f35d99d
qshome <758085249@qq.com>
2018-12-13 08:55:57 +0800

    项目搭建

---
+++ sys_parent/sys_api/src/main/java/com/sys/api/user/UserInterface.java
@@ -0,0 +1,7 @@
+package com.sys.api.user;
+
+import com.sys.api.base.BaseInterface;
+
+public interface UserInterface<T> extends BaseInterface<T>{
+
+}
---
+++ sys_parent/sys_entity/src/main/java/com/sys/entity/page/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.sys.entity.page;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 29956803811084609L;
+
+}
