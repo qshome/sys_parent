3a569447e67190ae5489b4ec6f976053c2f107da
qshome <758085249@qq.com>
2018-12-19 16:44:16 +0800

    添加定时任务

--- sys_parent/sys_server/src/main/java/com/sys/server/job/SpringTaskDemo.java
+++ sys_parent/sys_server/src/main/java/com/sys/server/job/SpringTaskDemo.java
@@ -19,4 +19,16 @@ public class SpringTaskDemo {
         Thread.sleep(3000);
         log.info("scheduled1 每1秒执行一次：{}", LocalDateTime.now());
     }
+	
+	@Scheduled(fixedRate = 1000)
+    public void scheduled2() throws InterruptedException {
+        Thread.sleep(3000);
+        log.info("scheduled2 每1秒执行一次：{}", LocalDateTime.now());
+    }
+	
+	@Scheduled(fixedDelay = 3000)
+    public void scheduled3() throws InterruptedException {
+        Thread.sleep(5000);
+        log.info("scheduled3 上次执行完毕后隔3秒继续执行：{}", LocalDateTime.now());
+    }
 }
